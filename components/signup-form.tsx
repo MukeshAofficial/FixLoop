23524765ba5fce94ab9a77b34c52194ec8bfb4bf
Mukesh <mukeshofficial685@gmail.com>
2025-08-09 23:07:51 +0530

    make signup functional

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,19 +1,52 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
+import { useState } from "react"
+import {createClient} from "@/lib/supabaseClient"
+
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [name,setName]=useState("")
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const[error,setError]=useState<string|null>(null)
+
+const handleSubmit=async(e:React.FormEvent)=>{
+  e.preventDefault()
+  const{data,error}=await createClient().auth.signUp({
+    email,
+    password,
+    options:{data:{display_name:name}}
+  })
+  if(error){
+    setError(error.message)
+  }
+}
+
+const handleGoogleSignup=async()=>{
+
+  const{data,error:GoogleError}=await createClient().auth.signInWithOAuth({
+    provider:"google",
+    options:{
+      redirectTo:`${window.location.origin}`
+    }
+  });
+  if(GoogleError){
+    setError(GoogleError.message)
+  };
+}
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome to Fixloop</h1>
@@ -26,6 +59,8 @@ export function SignupForm({
                 <Input
                   id="name"
                   type="text"
+                  value={name}
+                  onChange={e=>setName(e.target.value)}
                   required
                 />
               </div>
@@ -35,6 +70,8 @@ export function SignupForm({
                   id="email"
                   type="email"
                   placeholder="m@example.com"
+                  value={email}
+                  onChange={e=>setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -48,10 +85,11 @@ export function SignupForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input id="password" type="password" value={password} onChange={e=>setPassword(e.target.value)}required  />
               </div>
+              {error && <p className="text-red-500">{error}</p>}
               <Button type="submit" className="w-full">
-                Login
+                Signup
               </Button>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
@@ -68,7 +106,7 @@ export function SignupForm({
                   </svg>
                   <span className="sr-only">Login with Apple</span>
                 </Button>
-                <Button variant="outline" type="button" className="w-full">
+                <Button variant="outline" type="button" className="w-full" onClick={handleGoogleSignup}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -109,5 +147,5 @@ export function SignupForm({
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
-  )
+  );
 }
