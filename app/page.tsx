6454af12c1ba12db561d67f751790ea127d4523e
Mukesh <mukeshofficial685@gmail.com>
2025-08-10 22:16:33 +0530

    add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Navbar from "@/components/navbar";
+import {GlobeDemo} from "@/components/globe";
+import CoverDemo from "@/components/hero";
+
 
 export default function Home(){
   return(
     <main className="p-18">
       <Navbar/>
+      <div className="mt-8" >
+      <CoverDemo/>
+      </div>
+      <GlobeDemo />
+
+
     </main>
   )
 }
